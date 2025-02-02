2187e8b505a9f0d26d5dbc377f2f568ed9e1973f
pudin2 <josephbeltran777@gmail.com>
2025-02-02 15:21:01 -0500

    Carpeta Assets Agregada

--- app/src/main/java/com/example/horas_extra_tecnipalma/MainActivity.kt
+++ app/src/main/java/com/example/horas_extra_tecnipalma/MainActivity.kt
@@ -6,25 +6,35 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.horas_extra_tecnipalma.ui.theme.Horas_Extra_TecnipalmaTheme
-import androidx.compose.foundation.Image
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import com.example.horas_extra_tecnipalma.ui.theme.Horas_Extra_TecnipalmaTheme
-
-
-
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,13 +52,9 @@ fun LoginScreen(context: Context) {
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var selectedLocation by remember { mutableStateOf("") }
-
-
-    var isRegisterMode by remember { mutableStateOf(false) }
+    //var isRegisterMode by remember { mutableStateOf(false) }
     var showTextField by remember { mutableStateOf(false) }
     var additionalInfo by remember { mutableStateOf("") }
-
-
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -171,7 +177,6 @@ fun DropdownMenuExample(selectedLocation: String, onLocationSelected: (String) -
         )
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
